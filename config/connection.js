1603064c151c03fb79a75163dd3129c6ee22caa7
mikafeng <mikafengler@gmail.com>
2023-03-26 17:20:46 -0400

    fix and create get create update thought requests

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const connectionString =
     process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialmediadb';
 
 connect(connectionString, {
-
+    useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,14 +1,41 @@
 const { Thought, User } = require('../models');
 
 module.exports = {
-    getThoughts() {
+    getThoughts(req, res) {
         Thought.find()
         .then((thoughts) => res.json(thoughts))
+        .catch((err) => res.status(500).json(err))
+    },
+    getSingleThought(req, res) {
+        Thought.findOne({_id: req.params.thoughtId})
+        .then((thought) =>
+        !thought
+        ? res.status(404).json({message: 'No thought found with that id.'})
+        : res.json(thought)
+        )
         .catch((err) => res.status(500).json(err));
     },
-    getSingleThought() {},
-    createThought(){},
-    updateThought(){},
+    createThought(req, res){
+        Thought.create(req.body)
+        .then((dbThoughtData) => res.json(dbThoughtData))
+        .catch((err) => res.status(500).json(err))
+    },
+    updateThought(req, res){
+        Thought.findOneAndUpdate(
+            {_id: req.params.thoughtId},
+            {$set: req.body},
+            {runValidators: true, new: true}
+        )
+        .then((thought) =>
+        !thought
+        ? res.status(404).json('No thought with that id found.')
+        : res.json(thought)
+        )
+        .catch((err) => {
+        console.log(err)
+        res.status(500).json(err)
+        });
+    },
     deleteThought(){},
 }
 
