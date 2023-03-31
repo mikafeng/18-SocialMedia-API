dd293abdb310711980eda12a69aa2ceca77f6523
mikafeng <mikafengler@gmail.com>
2023-03-31 10:41:26 -0400

    time getter momment

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,6 @@
 const { Schema, model} = require('mongoose');
 const Reaction = require('./Reaction');
+const moment = require("moment");
 
 const thoughtSchema = new Schema(
     {
@@ -12,6 +13,8 @@ const thoughtSchema = new Schema(
         createdAt: {
             type: Date,
             default: Date.now(),
+            get: (createdAtVal) =>
+                moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a")
             //need getter method to format timestamp on query
         },
         username: {
@@ -23,6 +26,7 @@ const thoughtSchema = new Schema(
     {
         toJSON: {
             virtuals: true,
+            getters: true,
         },
 
         id: false,
